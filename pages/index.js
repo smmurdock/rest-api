fbc0defe6f7599d50ea49683077fcb4949c9b24c
Shanay Murdock <murdock.shanay@gmail.com>
2022-05-30 11:02:51 -0500

    Fill out the documentation for each endpoint

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,129 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const endpoints = [
+    {
+      name: 'GET /trips',
+      description: 'List all the trips',
+    },
+    {
+      name: 'POST /trips',
+      description: 'Create a new trip',
+      parameters: [
+				{ name: 'user', description: '*required* the id of the user' },
+        { name: 'name', description: '*required* the name of the trip' },
+        {
+          name: 'start_date',
+          description: '(optional) the starting date of the trip',
+        },
+        {
+          name: 'end_date',
+          description: '(optional) the end date of the trip',
+        },
+      ],
+    },
+    {
+      name: 'GET /trips/:id',
+      description: 'Get the details of a trip, including expenses',
+      parameters: [
+        {
+          name: 'id',
+          description: '*required* the number that identifies the trip',
+        },
+      ],
+      response: `
+
+      `,
+    },
+    {
+      name: 'PUT /trips/:id',
+      description: 'Edit a trip',
+      parameters: [
+        {
+          name: 'id',
+          description: '*required* the number that identifies the trip',
+        },
+        { name: 'name', description: '(optional) the name of the trip' },
+        {
+          name: 'start_date',
+          description: '(optional) the starting date of the trip',
+        },
+        {
+          name: 'end_date',
+          description: '(optional) the end date of the trip',
+        },
+      ],
+    },
+    {
+      name: 'DELETE /trips/:id',
+      description: 'Delete a trip',
+      parameters: [
+        {
+          name: 'id',
+          description: '*required* the number that identifies the trip',
+        },
+      ],
+    },
+    {
+      name: 'POST /expenses',
+      description: 'Create a new expense',
+      parameters: [
+        {
+          name: 'trip',
+          description: '*required* the number that identifies the trip',
+        },
+        { name: 'name', description: '*required* the name of the expense' },
+        { name: 'date', description: '*required* the date of the expense' },
+        { name: 'amount', description: '*required* the amount of the expense' },
+        {
+          name: 'currency',
+          description: '*required* the currency of the expense',
+        },
+      ],
+    },
+    {
+      name: 'GET /expenses/:id',
+      description: 'Get the details of an expense',
+      parameters: [
+        {
+          name: 'id',
+          description: '*required* the number that identifies the expense',
+        },
+      ],
+    },
+    {
+      name: 'PUT /expenses/:id',
+      description: 'Edit an expense',
+      parameters: [
+        {
+          name: 'id',
+          description: '*required* the number that identifies the expense',
+        },
+        {
+          name: 'trip',
+          description: '(optional) the number that identifies the trip',
+        },
+        { name: 'name', description: '(optional) the name of the expense' },
+        { name: 'date', description: '(optional) the date of the expense' },
+        { name: 'amount', description: '(optional) the amount of the expense' },
+        {
+          name: 'currency',
+          description: '(optional) the currency of the expense',
+        },
+      ],
+    },
+    {
+      name: 'DELETE /expense',
+      description: 'Delete an expense',
+      parameters: [
+        {
+          name: 'id',
+          description: '*required* the number that identifies the expense',
+        },
+      ],
+    },
+  ]
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,34 +138,36 @@ export default function Home() {
         <p className={styles.description}>The documentation</p>
 
         <div className={styles.grid}>
-          <div className={styles.card}>
-            <h2>
-              <code>GET /trips</code>
-            </h2>
-            <p>List all the trips</p>
-            <p>...</p>
-          </div>
-          <div className={styles.card}>
-            <h2>
-              <code>GET /trip</code>
-            </h2>
-            <p>Get the details of a trip</p>
-            <p>...</p>
-          </div>
-          <div className={styles.card}>
-            <h2>
-              <code>POST /trip</code>
-            </h2>
-            <p>Create a new trip</p>
-            <p>...</p>
-          </div>
-          <div className={styles.card}>
-            <h2>
-              <code>PUT /trip</code>
-            </h2>
-            <p>Edit new trip</p>
-            <p>...</p>
-          </div>
+          {endpoints.map((endpoint, index) => (
+            <div className={styles.card} key={index}>
+              <h2>
+                <code>{endpoint.name}</code>
+              </h2>
+              <p>{endpoint.description}</p>
+              {endpoint.parameters && (
+                <>
+                 <br />
+                 <p>Parameters:</p>
+                 <ul>
+                  {endpoint.parameters.map((parameter, parameterIndex) => (
+                    <li key={parameterIndex}>
+                      <b>{parameter.name}</b>: {parameter.description}
+                    </li>
+                  ))}
+                </ul>
+                </>
+              )}
+              {endpoint.response && (
+                <>
+                  <br />
+                  <p>Example response:</p>
+                  <pre>
+                    <code>{endpoint.response}</code>
+                  </pre>
+                </>
+              )}
+            </div>
+          ))}
         </div>
       </main>
     </div>
