cd02848272f2f0827741a66e3eec9ef3543ca595
Shanay Murdock <murdock.shanay@gmail.com>
2022-05-30 12:17:35 -0500

    Setup Prisma to retrieve data & send as a response

--- pages/api/trips/index.js
+++ pages/api/trips/index.js
@@ -1,11 +1,12 @@
-export default function handler(req, res) {
+import prisma from 'lib/prisma'
+
+export default async function handler(req, res) {
   // discards every request that's not using the 'GET' method
   if (req.method !== 'GET') {
-    res.status(405).json({
-      message: 'Method Not Allowed'
-    })
+    res.status(405).json({message: 'Method Not Allowed'})
     return
   }
 
-  res.status(200).json({ test: 'TRIPS!' })
+  const trips = await prisma.trip.findMany()
+  res.status(200).json(trips)
 }
