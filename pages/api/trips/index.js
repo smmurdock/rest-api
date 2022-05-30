78a7c1a4c7319521385d321230db4ffff77535ba
Shanay Murdock <murdock.shanay@gmail.com>
2022-05-30 11:44:53 -0500

    Setup /trips endpoint to only receive GET requests

--- pages/api/trips/index.js
+++ pages/api/trips/index.js
@@ -1,3 +1,11 @@
 export default function handler(req, res) {
-  res.status(200).json({test: 'TRIPS!'})
+  // discards every request that's not using the 'GET' method
+  if (req.method !== 'GET') {
+    res.status(405).json({
+      message: 'Method Not Allowed'
+    })
+    return
+  }
+
+  res.status(200).json({ test: 'TRIPS!' })
 }
